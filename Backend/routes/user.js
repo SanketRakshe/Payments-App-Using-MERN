c8f2673df2ff386917b5ad69e2cb03615adfcac1
sanketrakshe <sanketrakshe07@gmail.com>
2024-08-21 01:27:17 +0530

    FEAT: added signin functionality with JWT auth tested using postman

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -52,5 +52,41 @@ router.post("/signup", async (req, res) => {
 })
 
 
+const signinBody = zod.object({
+    username: zod.string().email(),
+    password: zod.string(),
+})
+
+router.post("/signin", async (req, res) => {
+    const { success } = signinBody.safeParse(req.body);
+
+    if(!success) {
+        return res.status(411).json({
+            message: "Email already taken / Invalid inputs"
+        });
+    }
+
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    if(user) {
+        const token = jwt.sign({
+            userId: user._id,
+        }, JWT_SECRET);
+
+        res.json({
+            token: token,
+            message: "User authenticated successfully"
+        })
+        return ;
+    }
+
+    res.status(411).json({
+        message: "Invalid username or password"
+    })
+})
+
 
 module.exports = router;
