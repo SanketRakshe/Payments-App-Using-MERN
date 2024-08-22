1cb74e8f1bb55204c26854065c323ee822674462
sanketrakshe <sanketrakshe07@gmail.com>
2024-08-22 15:45:54 +0530

    Added Routes and HEADING component

--- Frontend/Payments-App/src/App.jsx
+++ Frontend/Payments-App/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 function App() {
 
   return (
     <>
-      <p className='text-3xl font-bold underline'>Hi there</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='send' element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
