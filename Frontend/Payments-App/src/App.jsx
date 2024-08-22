c9c27659ae9edbe959628dd4af4da9be5e5269a4
sanketrakshe <sanketrakshe07@gmail.com>
2024-08-22 16:22:20 +0530

    Added: Components and a Sign In page

--- Frontend/Payments-App/src/App.jsx
+++ Frontend/Payments-App/src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {Signin} from './pages/signin'
 function App() {
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/signup' element={<Signup />} />
           <Route path='/signin' element={<Signin />} />
+          {/* <Route path='/signin' element={<Signin />} />
           <Route path='dashboard' element={<Dashboard />} />
-          <Route path='send' element={<SendMoney />} />
+          <Route path='send' element={<SendMoney />} /> */}
         </Routes>
       </BrowserRouter>
     </>
